750992e038e823355808bc0b44ecc78bc1d235c7
unknown <adrianyuson1988@gmail.com>
2019-01-29 00:49:06 -0800

    Initial commit

---
+++ COMP3951Lab3/Main.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace COMP3951Lab3
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openBrowserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeBrowserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.labelCurrentPath = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(564, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openBrowserToolStripMenuItem,
+            this.closeBrowserToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openBrowserToolStripMenuItem
+            // 
+            this.openBrowserToolStripMenuItem.Name = "openBrowserToolStripMenuItem";
+            this.openBrowserToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openBrowserToolStripMenuItem.Text = "Open Browser";
+            this.openBrowserToolStripMenuItem.Click += new System.EventHandler(this.openBrowserToolStripMenuItem_Click);
+            // 
+            // closeBrowserToolStripMenuItem
+            // 
+            this.closeBrowserToolStripMenuItem.Name = "closeBrowserToolStripMenuItem";
+            this.closeBrowserToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeBrowserToolStripMenuItem.Text = "Close Browser";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(0, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(564, 215);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // labelCurrentPath
+            // 
+            this.labelCurrentPath.AutoSize = true;
+            this.labelCurrentPath.Location = new System.Drawing.Point(13, 249);
+            this.labelCurrentPath.Name = "labelCurrentPath";
+            this.labelCurrentPath.Size = new System.Drawing.Size(0, 13);
+            this.labelCurrentPath.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 348);
+            this.Controls.Add(this.labelCurrentPath);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Adrian Yuson A00791498";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openBrowserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeBrowserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label labelCurrentPath;
+    }
+}
+
---
+++ COMP3951Lab3/Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP3951Lab3
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void openBrowserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (Dialog dialog = new Dialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.Yes)
+                {
+                    showCurrentDirectory();
+                }
+            }
+        }
+
+        private void DisplayFileSystemInfoAttributes(FileSystemInfo fsi)
+        {
+            //  Assume that this entry is a file.
+            string entryType = "File";
+
+            // Determine if entry is really a directory
+            if ((fsi.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                entryType = "Directory";
+            }
+            //  Show this entry's type, name, and creation date.
+            Console.WriteLine("{0} entry {1} was created on {2:D}", entryType, fsi.FullName, fsi.CreationTime);
+        }
+
+        private void showCurrentDirectory()
+        {
+            string parentDirectoryName = Path.GetFileName(Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)));
+            listView1.Items.Add(parentDirectoryName);
+            DisplayFileSystemInfoAttributes(new DirectoryInfo(parentDirectoryName));
+
+            //  Loop through all the immediate subdirectories of C.
+            foreach (string entry in Directory.GetDirectories(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)))
+            {
+                DisplayFileSystemInfoAttributes(new DirectoryInfo(entry));
+                listView1.Items.Add(entry);
+            }
+
+            //  Loop through all the files in C.
+            foreach (string entry in Directory.GetFiles(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)))
+            {
+                DisplayFileSystemInfoAttributes(new FileInfo(entry));
+                listView1.Items.Add(entry);
+            }
+            string currentDirectoryName = Path.GetFileName(Path.GetDirectoryName(@"C:\Users\adrian\Desktop"));
+            labelCurrentPath.Text = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        }
+
+    }
+}
